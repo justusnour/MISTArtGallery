eb1b10bec9a4e97f77b686f3592fdb287d654494
justusnour <justusnour@gmail.com>
2024-11-12 12:57:33 -0500

    Update Artwork.java

--- Artwork.java
+++ Artwork.java
@@ -1,4 +1,37 @@
 
 public class Artwork {
 
+	public String title;
+	public String artist;
+	public String price;
+	public boolean inStock;
+	
+	//getters + setters
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getArtist() {
+		return artist;
+	}
+	public void setArtist(String artist) {
+		this.artist = artist;
+	}
+	public String getPrice() {
+		return price;
+	}
+	public void setPrice(String price) {
+		this.price = price;
+	}
+	public boolean isAvailable() {
+		return inStock;
+	}
+	public void setAvailable(boolean inStock) {
+		this.inStock = inStock;
+	}
+	
+	
+	
 }
