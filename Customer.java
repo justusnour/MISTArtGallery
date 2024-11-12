d62eff48ebcb479db69375b19dfb5284680c6421
justusnour <justusnour@gmail.com>
2024-11-12 12:57:48 -0500

    Update Customer.java

--- Customer.java
+++ Customer.java
@@ -1,4 +1,30 @@
 
 public class Customer {
 
+	public String piecename;
+	public String artistname;
+	public double budget;
+	
+	
+	public String getPiecename() {
+		return piecename;
+	}
+	public void setPiecename(String piecename) {
+		this.piecename = piecename;
+	}
+	public String getArtistname() {
+		return artistname;
+	}
+	public void setArtistname(String artistname) {
+		this.artistname = artistname;
+	}
+	public double getBudget() {
+		return budget;
+	}
+	public void setBudget(double budget) {
+		this.budget = budget;
+	}
+	
+	
+	
 }
